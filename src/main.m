5cf3111270a1ec254e9d805e639046046db00aff
Jesper Lund <jeslu246@student.liu.se>
2018-04-27 15:33:25 +0200

    added number of checkins per ID functionality

--- src/main.m
+++ src/main.m
@@ -51,4 +51,62 @@ xlabel('x-coordinate')
 ylabel('y-coordinate')
 title('Frequency of X/Y coordinates in park map')
 
+%% How many unique visitors are there?
+
+[c2,~,~] = unique(parsed_data.id,'stable');
+numberUnique = length(c2);
+%c2 = sort(c2);
+
+%% Sort ID 
+
+[sorted_IDs, id_index] = sort(parsed_data.id);
+parsed_data_checkin = parsed_data.type(parsed_data.type=='check-in');
+
+%id_str = num2str(parsed_data.id);
+
+%id_type(:,1) = parsed_data.id;
+%id_type(:,2) = id_str;
+%IDs = c2;
+
+%% Create only checkin matrix
+counter = 1;
+for i=1:length(sorted_IDs)
+
+    if(parsed_data.type(i) == 'check-in')
+        data_checkin_coordinates(counter,1) = parsed_data.id(i);
+        data_checkin_coordinates(counter,2) = parsed_data.xCoordinates(i);
+        data_checkin_coordinates(counter,3) = parsed_data.yCoordinates(i);
+        counter = counter + 1;
+    end
+    
+end
+
+%% How many checkins do every person have?
+
+counter = 0;
+data_checkin_coordinates_sorted = sortrows(data_checkin_coordinates); 
+numberOfCheckin_perID = ones(numberUnique,2);
+
+index = 1;
+for i=1:(length(data_checkin_coordinates_sorted) -1)
+    
+    if(data_checkin_coordinates_sorted(i,1) == data_checkin_coordinates_sorted(i+1,1))
+        counter = counter + 1;   
+    else
+        numberOfCheckin_perID(index, 1) = data_checkin_coordinates_sorted(i,1);
+        numberOfCheckin_perID(index, 2) = counter;
+        index = index + 1;
+        counter = 0;
+   end
+    
+end    
+
+%% Plot number of checkins per ID
+
+figure;
+bar(numberOfCheckin_perID(:,1), numberOfCheckin_perID(:,2), 'BarWidth', 75);
+max_checkins = max(numberOfCheckin_perID(:,2));
+xlim([min(numberOfCheckin_perID(:,1)) max(numberOfCheckin_perID(:,1))]);
+ylim([0 max_checkins]);
+
 
